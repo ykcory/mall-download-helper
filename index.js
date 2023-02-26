b7a33d900d70979c5e9f988cfac262967939db8b
yang_xiaodong <yacoda@qq.com>
2023-02-26 12:51:28 +0800

    refactor: 💡 提取创建a标签的公共方法

--- index.js
+++ index.js
@@ -4,42 +4,69 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       ykcory
-// @match        *://item.jd.com/*
+// @match        *://(item.jd.com|detail.tmall.com)/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 (function () {
   "use strict";
-  const leftBtns = document.querySelector(".preview-wrap .left-btns");
-  // 添加下载图片按钮
-  const downTopImgBtn = document.createElement("a");
-  downTopImgBtn.href = "javascript:void(0)";
-  downTopImgBtn.innerText = "下载当前图片";
-  leftBtns.appendChild(downTopImgBtn);
-  downTopImgBtn.addEventListener("click", () => {
-    const img = document.querySelector(".preview-wrap #spec-img");
-    const imgUrl = img.src;
-    const bigImgUrl = imgUrl.replace("n1", "n12");
-    window.open(bigImgUrl);
-  });
-  // 添加下载视频按钮
-  const downVideoBtn = document.createElement("a");
-  downVideoBtn.href = "javascript:void(0)";
-  downVideoBtn.innerText = "下载视频";
-  leftBtns.appendChild(downVideoBtn);
-  downVideoBtn.addEventListener("click", () => {
-    // 播放视频
-    const previewBtn = document.querySelector(".preview-wrap .preview-btn .video-icon");
-    previewBtn.click();
-    // 获取播放按钮
-    setTimeout(()=>{
-        const video = document.querySelector(".preview-wrap .J-v-player #video-player_html5_api source");
-        console.log(video.src);
-        if(video){
-            window.open(video.src)
+  const currentHref = window.location.href;
+  const isJd = currentHref.includes("item.jd.com");
+  const isTm = currentHref.includes("detail.tmall.com");
+  if (isJd) {
+    jdDownload()
+  }
+  if (isTm) {
+    tmDownload()
+  }
+
+  /**
+     * 京东图片下载
+     */
+  function jdDownload() {
+    const leftBtns = document.querySelector(".preview-wrap .left-btns");
+    // 添加下载图片按钮
+    const downTopImgBtn = createATag("下载当前图片")
+    leftBtns.appendChild(downTopImgBtn);
+    downTopImgBtn.addEventListener("click", () => {
+      const img = document.querySelector(".preview-wrap #spec-img");
+      const imgUrl = img.src;
+      const bigImgUrl = imgUrl.replace("n1", "n12");
+      window.open(bigImgUrl);
+    });
+    // 添加下载视频按钮
+    const downVideoBtn = createATag("下载视频")
+    leftBtns.appendChild(downVideoBtn);
+    downVideoBtn.addEventListener("click", () => {
+      // 播放视频
+      const previewBtn = document.querySelector(
+        ".preview-wrap .preview-btn .video-icon"
+      );
+      previewBtn.click();
+      // 获取播放按钮
+      setTimeout(() => {
+        const video = document.querySelector(
+          ".preview-wrap .J-v-player #video-player_html5_api source"
+        );
+        if (video) {
+          window.open(video.src);
         }
-    },500)
-    
-});
+      }, 500);
+    });
+  }
+  /**
+   * 天猫图片下载
+   */
+  function tmDownload(){
+    console.log("天猫")
+  }
+  /**
+   * 创建 a 标签
+   */
+  function createATag(innerText){
+    const aTag = document.createElement("a");
+    aTag.href = "javascript:void(0)";
+    aTag.innerText = innerText;
+  }
 })();
